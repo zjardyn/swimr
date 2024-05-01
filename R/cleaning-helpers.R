11e69095aa0c93d33877ea162893dd0abf8e71c7
zjardyn <zlieraho@uwaterloo.ca>
2024-05-01 19:13:23 -0400

    delta

--- R/cleaning-helpers.R
+++ R/cleaning-helpers.R
@@ -1,20 +1,27 @@
-library(tidyverse)
+# option 1 (then you should also put utils in Imports)
+utils::globalVariables(c("english", "temp"))
+
+# option 2
+# english <- temp <- NULL
+
+lookup_table <- dplyr::tribble(
+    ~where, ~english,
+    "beach",     "US",
+    "coast",     "US",
+    "seashore",     "UK",
+    "seaside",     "UK"
+)
 
 #' @export
 localize_beach <- function(dat) {
-  lookup_table <- read_csv(
-    "beach-lookup-table.csv",
-    col_types = cols(where = "c", english = "c")
-  )
-  left_join(dat, lookup_table)
+    dplyr::left_join(dat, lookup_table)
 }
 
-#' @export
 f_to_c <- function(x) (x - 32) * 5/9
 
 #' @export
 celsify_temp <- function(dat) {
-  mutate(dat, temp = if_else(english == "US", f_to_c(temp), temp))
+    dplyr::mutate(dat, temp = dplyr::if_else(english == "US", f_to_c(temp), temp))
 }
 
 now <- Sys.time()
@@ -22,5 +29,5 @@ timestamp <- function(time) format(time, "%Y-%B-%d_%H-%M-%S")
 
 #' @export
 outfile_path <- function(infile) {
-  paste0(timestamp(now), "_", sub("(.*)([.]csv$)", "\\1_clean\\2", infile))
+    paste0(timestamp(now), "_", sub("(.*)([.]csv$)", "\\1_clean\\2", infile))
 }
